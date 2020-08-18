a4558468fcc9f627d7e4b60c04143cf1eab20826
Fizx <kazanfizuku@gmail.com>
2020-08-18 18:41:07 +0800

    Fixed Bugs

--- events/level.js
+++ events/level.js
@@ -3,29 +3,34 @@ module.exports = {
   name: "message",
   run: async (message, client) => {
     
-    const db = client.db;
+  const db = client.db;
     
-    if (message.channel.type == "dm") return; // Prevents DMs
-    if (message.author.bot) return; // Prevents bots
+  if (message.channel.type == "dm") return; // Prevents DMs
+  if (message.author.bot) return; // Prevents bots
     
-    xp(message)
+  xp(message)
     
   async function xp(message) {
     try {
       if (client.cooldown.has(message.author.id)) return;
-      client.cooldown.add(message.author.id);
-      const randomXP = Math.floor(Math.random() * 14) + 1;
+      //client.cooldown.add(message.author.id);
       let xp = await db.get(`xp_${message.guild.id}_${message.author.id}`);
-      if (xp == null) await db.set(`xp_${message.guild.id}_${message.author.id}`, "1")
-      await db.set(`xp_${message.guild.id}_${message.author.id}`, `${parseInt(xp) + randomXP}`);
+      if (xp == "null") await db.set(`xp_${message.guild.id}_${message.author.id}`, 1)
+      let newXP = await db.get(`xp_${message.guild.id}_${message.author.id}`);
+      const randomXP = Math.floor(Math.random() * 14) + 1;
+      let addedXP = parseInt(newXP) + randomXP
+      await db.set(`xp_${message.guild.id}_${message.author.id}`, addedXP);
       let level = await db.get(`level_${message.guild.id}_${message.author.id}`);
-      if (level == null) await db.set(`level_${message.guild.id}_${message.author.id}`, "1")
-      let nexp = Math.floor(Math.pow(parseInt(level) / 0.1, 2));
+      if (level == "null") await db.set(`level_${message.guild.id}_${message.author.id}`, 1)
+      let newLvl = await db.get(`level_${message.guild.id}_${message.author.id}`);
+      let nexp = Math.floor(Math.pow(parseInt(newLvl) / 0.1, 2));
       let exp =
         (await db.get(`xp_${message.guild.id}_${message.author.id}`)) ||
-        db.set(`level_${message.guild.id}_${message.author.id}`, "1");
+        db.set(`level_${message.guild.id}_${message.author.id}`, 1);
       if (exp > nexp) {
-        await db.set(`level_${message.guild.id}_${message.author.id}`, `${parseInt(level) + 1}`);
+        let anothaLvl = await db.get(`level_${message.guild.id}_${message.author.id}`);
+        let newestLvl = parseInt(anothaLvl) + 1
+        await db.set(`level_${message.guild.id}_${message.author.id}`, newestLvl);
         let newLevel = await db.get(`level_${message.guild.id}_${message.author.id}`);
         message.reply(`you leveled up to ${newLevel}! GG!`);
       }
